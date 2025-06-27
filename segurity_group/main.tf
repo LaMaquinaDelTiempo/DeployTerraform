299a4effd405f88dfe0a89fd9b0909efd50f4579
DiegoHernandez <diego0930@gmail.com>
2025-06-26 23:16:04 -0500

    ingres and egress

---
+++ segurity_group/main.tf
@@ -0,0 +1,42 @@
+resource "aws_security_group" "my_sg" {
+    
+    vpc_id      = var.vpc_id
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "Allow HTTPS traffic"
+        from_port   = 5173
+        to_port     = 5173
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        description = "Allow all outbound traffic"
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "security-group"
+    }
+}
+
+variable "vpc_id" {
+    description = "The ID of the VPC"
+    type        = string
+}
