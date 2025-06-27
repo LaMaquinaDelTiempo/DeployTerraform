7a1db053e3d1008fa21d8404cca9033989beb25d
DiegoHernandez <diego0930@gmail.com>
2025-06-27 14:23:40 -0500

    c1

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
 module "app_server" {
-  source         = "./modules/ec2"
-  ami            = var.ec2_ami
-  instance_type  = var.ec2_instance_type
-  key_name       = var.key_name
-  sg_id          = var.sg_id
-  environment    = "prod"
+  source = "./modules/security_group"
+  vpc_id=var.vpc_id
 }
