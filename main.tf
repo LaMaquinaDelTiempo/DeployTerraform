eebc29ddc939d45e3982e4ce5bcc520f8facfd64
DiegoHernandez <diego0930@gmail.com>
2025-07-01 23:46:50 -0500

    c2

--- main.tf
+++ main.tf
@@ -6,3 +6,41 @@ module "app_server" {
   sg_id          = var.security_group_id
   environment    = "prod"
 }
+module "security_group" {
+  source            = "./modules/security_group"
+  security_group_name = "app-server-sg"
+  vpc_id            = var.vpc_id
+  ingress_rules     = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = [var.my_ip_cidr]
+    },
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 5000
+      to_port     = 5000
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 5173
+      to_port     = 5173
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    } ]
+    egress_rules = [
+    {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+    ]
+}
